ec2b53631c5de8f4f58d171846e650e894f35b66
Samrath Chadha <69572668+samrath2007@users.noreply.github.com>
2021-06-01 15:55:41 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,13 +4,13 @@ abbr c "clear"
 abbr fn "rm -rf ~/.local/share/nvim/swap"
 abbr ll 'ls'
 abbr l 'ls'
+abbr v 'nvim'
 abbr g 'git'
 abbr gb 'git branch'
 abbr ga 'git add'
 abbr gs 'git status -s'
 abbr gco 'git checkout'
-abbr gc 'git commit'
-abbr gcm 'git commit -m'
+abbr gcm 'git commit'
 abbr gca 'git commit -am'
 abbr gd 'git diff'
 abbr gp 'git push'
@@ -24,9 +24,14 @@ abbr gsw 'git switch'
 abbr gsb 'git switch (git base)'
 abbr gsm 'git switch master'
 abbr gsp 'git switch -'
+abbr gc 'git clone'
 abbr '....' '../../..'
 abbr '.....' '../../../..'
 abbr '......' '../../../../..'
+abbr 'ls' 'logo-ls -1 -A -D'
+abbr 'vrc' 'vim ~/.config/nvim/init.vim'
+abbr 'frc' 'vim ~/.config/fish/config.fish'
+
 
 function push
     git add .
@@ -37,6 +42,7 @@ funcsave push
 
 
 function fish_greeting
+	python3 ~/.greet.py
 end
 funcsave fish_greeting
 
@@ -61,8 +67,4 @@ function pull
   git pull
 end
 funcsave pull
-# i suggest running one of the following commands after installing fish
-# omf install bobthefish
-# OR
-# omf install sashimi
 fish_vi_key_bindings
