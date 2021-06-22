17324e378b001b47a44fdb887fd6876e2fcb7e7c
samrath2007 <samrath1324@gmail.com>
2021-06-22 10:06:50 +0530

    (chore) updated installer grammar

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ fi
 cp -r init.lua ~/.config/nvim/ && cp -r maps.vim ~/.config/nvim/ && cp -r lua ~/.config/nvim/
 
 
-echo "neovim will open with some errors , just press enter" && sleep 1
+echo "neovim will open with some errors, just press enter" && sleep 1
 
 # install all plugins
 nvim +PackerInstall 
