9f004610107a6839ae4a3dbcea4f1313e34849cd
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-07-21 20:07:38 +0800

    add patch for users who experience issues with nvim-tree.lua

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,20 @@ g.user_emmet_leader_key = ","
 g.nvim_tree_side = 'right'
 g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+
+-- uncomment this out if you are experiencing issues with nvim-tree.lua
+-- g.kyoto_nvimtree_patch = 1
+
+if os.getenv("GIT") == "0" or g.kyoto_nvimtree_patch == 1 then
+  g.nvim_tree_git_hl = 0
+  g.nvim_tree_gitignore = 0
+  g.nvim_tree_show_icons = {
+      git = 0,
+      folders = 1,
+      files = 1
+  }
+end
+
 opt.ruler = false
 opt.hidden = true
 opt.ignorecase = true
