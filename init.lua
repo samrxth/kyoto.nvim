ba4e383ffdd15a24021264c01bca36d1436ce3ee
samrath2007 <samrath1324@gmail.com>
2021-07-22 12:04:10 +0530

    (breaking change) completed rebuild, only formatting and lazy load remains

--- init.lua
+++ init.lua
@@ -2,11 +2,26 @@ require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'lambdalisue/fern.vim'
   use 'lambdalisue/nerdfont.vim'
+  use "onsails/lspkind-nvim"
+  use "kyazdani42/nvim-tree.lua"
+  use "glepnir/lspsaga.nvim"
+  use "kabouzeid/nvim-lspinstall"
+  use "nvim-treesitter/nvim-treesitter"
+  use "hrsh7th/nvim-compe"
+  use "neovim/nvim-lspconfig"
   use 'lambdalisue/fern-renderer-nerdfont.vim'
   use 'folke/tokyonight.nvim'
   use {
     'hoob3rt/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}
+  }
+  use {
+    "akinsho/nvim-bufferline.lua",
+    requires = "kyazdani42/nvim-web-devicons"
   }
 end)
 local opt = vim.opt
@@ -34,9 +49,8 @@ opt.shiftwidth = 2
 opt.smartindent = true
 opt.background = "dark"
 
-require("mappings")
-
 vim.cmd [[
+nnoremap <leader>nn :Fern -drawer . -toggle<CR>
 let g:fern#renderer = "nerdfont"
 function! Init_fern() abort
   nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:select)
@@ -49,6 +63,84 @@ augroup fern-custom
   autocmd FileType fern call Init_fern()
 augroup END
 colorscheme tokyonight
+if bufwinnr(1)
+  nnoremap <silent> <C-H> :vertical resize -4<CR>
+  nnoremap <silent> <C-L> :vertical resize +4><CR>
+  nnoremap <silent> <C-J> :resize -4<CR>
+  nnoremap <silent> <C-K> :resize +4<CR>
+  tnoremap <silent> <C-H> :vertical resize -4<CR>
+  tnoremap <silent> <C-L> :vertical resize +4><CR>
+  tnoremap <silent> <C-J> :resize -4<CR>
+  tnoremap <silent> <C-K> :resize +4<CR>
+endif
+
+nnoremap <leader>dc :e ~/.config/nvim/init.lua<CR>
+
+" Telescope
+nnoremap <leader>ff :Telescope find_files hidden=true<CR>
+nnoremap <leader>fb :Telescope buffers<CR>
+nnoremap <leader>fh :Telescope help_tags<CR>
+nnoremap <leader>fi :Telescope live_grep<CR>
+nnoremap <leader>fg :Telescope git_status<CR>
+nnoremap <leader>fo :Telescope oldfiles<CR>
+nnoremap <leader>fn :Telescope file_browser hidden=true<CR>
+
+map <Up> <NOP>
+map <Down> <NOP>
+map <Left> <NOP>
+map <Right> <NOP>
+
+nnoremap <leader>` :call ChooseTerm("term-slider", 1)<CR>
+nnoremap <leader><CR> :call ChooseTerm("term-pane", 0)<CR>
+ 
+function! ChooseTerm(termname, slider)
+    let pane = bufwinnr(a:termname)
+    let buf = bufexists(a:termname)
+    if pane > 0
+        " pane is visible
+        if a:slider > 0
+            :exe pane . "wincmd c"
+        else
+            :exe "e #" 
+        endif
+    elseif buf > 0
+        " buffer is not in pane
+        if a:slider
+            :exe "botright split"
+        endif
+        :exe "buffer " . a:termname
+    else
+        " buffer is not loaded, create
+        if a:slider
+            :exe "botright split"
+        endif
+        :terminal
+        :exe "f " a:termname
+    endif
+endfunction
+
+" compe completion
+inoremap <silent><expr> <CR>      compe#confirm('<CR>')
+inoremap <silent><expr> <C-e>     compe#close('<C-e>')
+inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
+inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
+inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+" Lspsaga
+nnoremap <silent> gh <cmd>lua require'lspsaga.provider'.lsp_finder()<CR>
+nnoremap <silent><leader>ca <cmd>lua require('lspsaga.codeaction').code_action()<CR>
+vnoremap <silent><leader>ca :<C-U>lua require('lspsaga.codeaction').range_code_action()<CR>
+nnoremap <silent> K <cmd>lua require('lspsaga.hover').render_hover_doc()<CR>
+nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
+nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
+nnoremap <silent> gs <cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>
+nnoremap <silent>gr <cmd>lua require('lspsaga.rename').rename()<CR>
+nnoremap <silent> gd <cmd>lua require'lspsaga.provider'.preview_definition()<CR>
+nnoremap <silent><leader>cd <cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>
+nnoremap <silent><leader>cc <cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>
+nnoremap <silent> [e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>
+nnoremap <silent> ]e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>
 ]]
 
 local function clock() return " " .. os.date("%H:%M") end
@@ -92,3 +184,8 @@ require('lualine').setup {
         lualine_z = {}
     },
 }
+
+require "top-bufferline"
+require "compe-config"
+require "telescope-conf"
+require "lsp-config"
---
+++ lua/compe-config.lua
@@ -0,0 +1,26 @@
+local vim = vim
+vim.o.completeopt = "menu,menuone,noselect"
+
+require "compe".setup {
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = "enable",
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  allow_prefix_unmatch = false,
+  with_text = false,
+  source = {
+    path = true,
+    buffer = true,
+    calc = true,
+    ultisnips = true,
+    vsnip = false,
+    nvim_lsp = true,
+    nvim_lua = true,
+    spell = true,
+    tags = true
+  }
+}
