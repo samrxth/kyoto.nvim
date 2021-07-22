70bec0e7708fc6a0ea62b57dbe7757a6919d44a7
samrath2007 <samrath1324@gmail.com>
2021-07-22 10:10:35 +0530

    (feat) base rebuild init

--- init.lua
+++ init.lua
@@ -1,12 +1,49 @@
-require "pluginList"
-require "options"
-require "statusline"
-require "top-bufferline"
-require "dashboard-conf"
-require "telescope-conf"
-require "general"
-require "au"
-require "lsp-config"
-require "compe-config"
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'lambdalisue/fern.vim'
+  use 'lambdalisue/nerdfont.vim'
+  use 'lambdalisue/fern-renderer-nerdfont.vim'
+  use 'folke/tokyonight.nvim'
+end)
+local opt = vim.opt
+local g = vim.g
 
-vim.cmd("source ~/.config/nvim/mappings.vim")
+g.kyoto_nvim_tree_show_git = false
+g.mapleader = " "
+
+vim.g.tokyonight_style = "storm"
+
+opt.ruler = false
+opt.hidden = true
+opt.ignorecase = true
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.cul = true
+opt.mouse = "a"
+opt.signcolumn = "yes"
+opt.cmdheight = 1
+opt.updatetime = 250 -- update interval for gitsigns
+opt.timeoutlen = 400
+opt.relativenumber = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.background = "dark"
+
+vim.cmd [[
+nnoremap <leader>nn :Fern -drawer .<CR>
+let g:fern#renderer = "nerdfont"
+function! Init_fern() abort
+  nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:select)
+  nmap <buffer> <CR> <Plug>(fern-action-open-or-expand)
+  nmap <buffer> d <Plug>(fern-action-remove)
+  setlocal nonumber norelativenumber 
+endfunction
+
+augroup fern-custom
+  autocmd! *
+  autocmd FileType fern call Init_fern()
+augroup END
+colorscheme tokyonight
+]]
