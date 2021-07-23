b2a0a89187f68c2a7f21d1269f409a0537bf26c6
samrath2007 <samrath1324@gmail.com>
2021-07-23 20:17:55 +0530

    feat: improved code quality

--- init.lua
+++ init.lua
@@ -1,19 +1,33 @@
 local vim = vim
 
+-- auto-commands and relative numbers
 vim.cmd [[ source ~/.config/nvim/viml/general.vim ]]
+-- general configurations
 require "options"
+-- user configurations
 require "kyotorc"
+-- startscreen
 require "dashboard-config"
 
+-- load after startup
 vim.defer_fn(function()
+  -- lualine configuration
   require "statusline"
+  -- nvim-bufferline.lua configuration
   require "top-bufferline"
+  -- autocomplete configuration
   require "compe-config"
+  -- fuzzy finder configuration
   require "telescope-config"
+  -- Langauge serer configuration
   require "lsp-config"
+  -- Git changes(showing in line number) configuration
   require "gitsigns-config"
+  -- configuration to help you remember keybindings
   require "which-key-config"
+  -- we can load packer last since we already have it compiled
   require "plugins"
+  -- source our mappings last(may change)
   vim.cmd [[ source ~/.config/nvim/viml/maps.vim ]]
 end, 0)
 
