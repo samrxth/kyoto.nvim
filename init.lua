94092d9855353023e14c5dd465d268fa36728e9c
samrath2007 <samrath1324@gmail.com>
2021-07-26 14:49:50 +0530

    feat: lazy load + comments in init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,32 @@
 local vim = vim
 
-require "kyotorc"
+-- auto-commands and relative numbers
+vim.cmd [[ source ~/.config/nvim/viml/general.vim ]]
+-- general configurations
 require "options"
-require "statusline"
-require "top-bufferline"
-require "compe-config"
-require "telescope-config"
-require "lsp-config"
+-- user configurations
+require "kyotorc"
+-- startscreen
 require "dashboard-config"
-require "gitsigns-config"
-require "which-key-config"
 
+-- load after startup
 vim.defer_fn(function()
-  require("plugins")
+  -- lualine configuration
+  require "statusline"
+  -- nvim-bufferline.lua configuration
+  require "top-bufferline"
+  -- autocomplete configuration
+  require "compe-config"
+  -- fuzzy finder configuration
+  require "telescope-config"
+  -- Langauge serer configuration
+  require "lsp-config"
+  -- Git changes(showing in line number) configuration
+  require "gitsigns-config"
+  -- configuration to help you remember keybindings
+  require "which-key-config"
+  -- we can load packer last since we already have it compiled
+  require "plugins"
+  -- source our mappings last(may change)
+  vim.cmd [[ source ~/.config/nvim/viml/maps.vim ]]
 end, 0)
-
-vim.cmd [[ source ~/.config/nvim/viml/maps.vim ]]
---
+++ viml/general.vim
@@ -0,0 +1,10 @@
+augroup terminalsettings
+	autocmd!
+	if has('nvim')
+		autocmd TermOpen *
+			\ setlocal nonumber norelativenumber |
+			\ startinsert
+	endif
+augroup end
+
+set nu rnu
