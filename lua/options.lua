4a751dfbc39628dbf6df101ebc6fa5eb74f8ef78
samrath2007 <samrath1324@gmail.com>
2021-07-30 14:20:50 +0530

    feat: disable some inbuilt plugins

--- lua/options.lua
+++ lua/options.lua
@@ -48,3 +48,28 @@ end
 vim.cmd [[
   colorscheme tokyonight
 ]]
+
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    g["loaded_" .. plugin] = 1
+end
