c30ae4bea006185fa8206d1d8450a0e776311cf1
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-08-05 13:32:35 -0300

    Added options to customize LSP behaviour

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,18 +1,47 @@
-local vim = vim
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require "lspinstall".setup() -- important
-require "lsp_signature".setup()
-
-local servers = require "lspinstall".installed_servers()
-for _, server in pairs(servers) do
-  require "lspconfig"[server].setup {}
-  require "lsp_signature".on_attach()
-end
 
 vim.fn.sign_define("LspDiagnosticsSignError", {text = "", numhl = "LspDiagnosticsDefaultError"})
 vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", numhl = "LspDiagnosticsDefaultWarning"})
 vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", numhl = "LspDiagnosticsDefaultInformation"})
 vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnosticsDefaultHint"})
+
+local function common_on_attach()
+  require "lsp_signature".on_attach {
+    bind = true
+  }
+end
+
+local configs = {
+  lua = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = {"vim"}
+        }
+      }
+    }
+  }
+}
+
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    local client = require'lspconfig'[server]
+    local config = configs[server] or client
+    client.setup {
+      filetypes = config.filetypes or client.filetypes,
+      on_attach = common_on_attach,
+      settings  = config.settings or {}
+    }
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd('bufdo e') -- this triggers the FileType autocmd that starts the server
+end
