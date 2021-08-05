459112dfe6dd55752ba36e162fdbbac4bf31e178
samrath2007 <samrath1324@gmail.com>
2021-08-05 22:44:45 +0530

    feat: added support for global lsp-config

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -7,41 +7,29 @@ vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", numhl = "LspD
 vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnosticsDefaultHint"})
 
 local function common_on_attach()
-  require "lsp_signature".on_attach {
-    bind = true
-  }
+    require "lsp_signature".on_attach {
+        bind = true
+    }
 end
 
-local configs = {
-  lua = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = {"vim"}
-        }
-      }
-    }
-  }
-}
-
 local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    local client = require'lspconfig'[server]
-    local config = configs[server] or client
-    client.setup {
-      filetypes = config.filetypes or client.filetypes,
-      on_attach = common_on_attach,
-      settings  = config.settings or {}
-    }
-  end
+    require "lspinstall".setup()
+    local servers = require "lspinstall".installed_servers()
+    for _, server in pairs(servers) do
+        local client = require "lspconfig"[server]
+        local config = vim.g.kyoto_lsp_configs[server] or client
+        client.setup {
+            filetypes = config.filetypes or client.filetypes,
+            on_attach = common_on_attach,
+            settings = config.settings or {}
+        }
+    end
 end
 
 setup_servers()
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd('bufdo e') -- this triggers the FileType autocmd that starts the server
+require "lspinstall".post_install_hook = function()
+    setup_servers() -- reload installed servers
+    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
