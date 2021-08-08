edb179e2dd15b627bbb6fb9dc6530e9fc64b776a
samrath2007 <samrath1324@gmail.com>
2021-08-08 11:37:02 +0530

    feat: replace lspkind with inbuilt definitions for symbols

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -3,16 +3,68 @@ local vim = vim
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require "lspinstall".setup() -- important
-require "lsp_signature".setup()
+require("lspinstall").setup() -- important
+require("lsp_signature").setup()
 
-local servers = require "lspinstall".installed_servers()
+local servers = require("lspinstall").installed_servers()
 for _, server in pairs(servers) do
-  require "lspconfig"[server].setup {}
-  require "lsp_signature".on_attach()
+  require("lspconfig")[server].setup({})
+  require("lsp_signature").on_attach()
 end
 
-vim.fn.sign_define("LspDiagnosticsSignError", {text = "", numhl = "LspDiagnosticsDefaultError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", numhl = "LspDiagnosticsDefaultWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", numhl = "LspDiagnosticsDefaultInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnosticsDefaultHint"})
+vim.fn.sign_define(
+  "LspDiagnosticsSignError",
+  { text = "", numhl = "LspDiagnosticsDefaultError" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignWarning",
+  { text = "", numhl = "LspDiagnosticsDefaultWarning" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignInformation",
+  { text = "", numhl = "LspDiagnosticsDefaultInformation" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignHint",
+  { text = "", numhl = "LspDiagnosticsDefaultHint" }
+)
+
+-- set default prefix.
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    -- virtual_text = false,
+    virtual_text = { prefix = "" },
+    signs = true,
+    update_in_insert = false,
+  }
+)
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+  "   (Text) ",
+  "   (Method)",
+  "   (Function)",
+  "   (Constructor)",
+  " ﴲ  (Field)",
+  "[] (Variable)",
+  "   (Class)",
+  " ﰮ  (Interface)",
+  "   (Module)",
+  " 襁 (Property)",
+  "   (Unit)",
+  "   (Value)",
+  " 練 (Enum)",
+  "   (Keyword)",
+  "   (Snippet)",
+  "   (Color)",
+  "   (File)",
+  "   (Reference)",
+  "   (Folder)",
+  "   (EnumMember)",
+  " ﲀ  (Constant)",
+  " ﳤ  (Struct)",
+  "   (Event)",
+  "   (Operator)",
+  "   (TypeParameter)",
+}
--- lua/pluginList.lua
+++ lua/pluginList.lua
@@ -1,91 +1,88 @@
 local vim = vim
 
-require("packer").startup(
-  function(use)
-    use "wbthomason/packer.nvim"
-    use "onsails/lspkind-nvim"
-    use {"kyazdani42/nvim-tree.lua", opt = true, cmd = {"NvimTreeToggle"}}
+require("packer").startup(function(use)
+  use("wbthomason/packer.nvim")
+  use({ "kyazdani42/nvim-tree.lua", opt = true, cmd = { "NvimTreeToggle" } })
 
-    use "glepnir/lspsaga.nvim"
-    use "kabouzeid/nvim-lspinstall"
-    use "nvim-treesitter/nvim-treesitter"
-    use "hrsh7th/nvim-compe"
-    use "neovim/nvim-lspconfig"
-    use "folke/tokyonight.nvim"
-    use {
-      "hoob3rt/lualine.nvim",
-      requires = {"kyazdani42/nvim-web-devicons", opt = true}
-    }
+  use("glepnir/lspsaga.nvim")
+  use("kabouzeid/nvim-lspinstall")
+  use("nvim-treesitter/nvim-treesitter")
+  use("hrsh7th/nvim-compe")
+  use("neovim/nvim-lspconfig")
+  use("folke/tokyonight.nvim")
+  use({
+    "hoob3rt/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
 
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}
-    }
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+  })
 
-    use {
-      "akinsho/nvim-bufferline.lua",
-      requires = "kyazdani42/nvim-web-devicons"
-    }
+  use({
+    "akinsho/nvim-bufferline.lua",
+    requires = "kyazdani42/nvim-web-devicons",
+  })
 
-    use "jiangmiao/auto-pairs"
-    use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons"
-    }
+  use("jiangmiao/auto-pairs")
+  use({
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+  })
 
-    use {
-      "glepnir/dashboard-nvim",
-      opt = true,
-      cmd = {
-        "Dashboard",
-        "DashboardChangeColorscheme",
-        "DashboardFindFile",
-        "DashboardFindHistory",
-        "DashboardFindWord",
-        "DashboardJumpMarks",
-        "DashboardNewfile"
-      }
-    }
+  use({
+    "glepnir/dashboard-nvim",
+    opt = true,
+    cmd = {
+      "Dashboard",
+      "DashboardChangeColorscheme",
+      "DashboardFindFile",
+      "DashboardFindHistory",
+      "DashboardFindWord",
+      "DashboardJumpMarks",
+      "DashboardNewfile",
+    },
+  })
 
-    use {
-      "lewis6991/gitsigns.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim"
-      }
-    }
+  use({
+    "lewis6991/gitsigns.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+  })
 
-    use "simrat39/symbols-outline.nvim"
-    use "folke/which-key.nvim"
-    use "tpope/vim-commentary"
-    use "kdheepak/lazygit.nvim"
-    use "yuttie/comfortable-motion.vim"
-    use {
-      "mhartington/formatter.nvim",
-      opt = true
-    }
-    use {
-      "folke/twilight.nvim",
-      opt = true
-    }
-    use "gelguy/wilder.nvim"
-    use {"maxmellon/vim-jsx-pretty", opt = true}
-    use {
-      "folke/zen-mode.nvim",
-      opt = true
-    }
+  use("simrat39/symbols-outline.nvim")
+  use("folke/which-key.nvim")
+  use("tpope/vim-commentary")
+  use("kdheepak/lazygit.nvim")
+  use("yuttie/comfortable-motion.vim")
+  use({
+    "mhartington/formatter.nvim",
+    opt = true,
+  })
+  use({
+    "folke/twilight.nvim",
+    opt = true,
+  })
+  use("gelguy/wilder.nvim")
+  use({ "maxmellon/vim-jsx-pretty", opt = true })
+  use({
+    "folke/zen-mode.nvim",
+    opt = true,
+  })
 
-    use {"tweekmonster/startuptime.vim", opt = true}
+  use({ "tweekmonster/startuptime.vim", opt = true })
 
-    use {"vimwiki/vimwiki", opt = true}
+  use({ "vimwiki/vimwiki", opt = true })
 
-    use "ray-x/lsp_signature.nvim"
+  use("ray-x/lsp_signature.nvim")
 
-    use {"creativenull/diagnosticls-nvim", opt = true}
+  use({ "creativenull/diagnosticls-nvim", opt = true })
 
-    use {"folke/lua-dev.nvim", opt = true}
+  use({ "folke/lua-dev.nvim", opt = true })
 
-    for _, plugin in ipairs(vim.g.kyoto_extra_plugins) do
-      use(plugin)
-    end
+  for _, plugin in ipairs(vim.g.kyoto_extra_plugins) do
+    use(plugin)
   end
-)
+end)
