31f9f92b207501b486cf49f185f26213437a24fa
Pyther99 <finngaertner2@gmx.de>
2021-09-14 22:05:23 +0200

    feat: Better DapBreakpoint symbol

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,5 +1,12 @@
 require("dapui").setup()
 
+vim.fn.sign_define('DapBreakpoint', {
+    text = '',
+    texthl = 'DapBreakpoint',
+    linehl = '',
+    numhl = ''
+})
+
 local dap_install = require("dap-install")
 local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
 
