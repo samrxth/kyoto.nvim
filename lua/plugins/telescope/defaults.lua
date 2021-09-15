12887df5fe210465074ddbd42521bf3bc1515e49
samrath2007 <samrath1324@gmail.com>
2021-09-15 22:34:29 +0530

    refactor: organize telescope config

---
+++ lua/plugins/telescope/defaults.lua
@@ -0,0 +1,48 @@
+local defaults = {
+  vimgrep_arguments = {
+    "rg",
+    "--color=never",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+  },
+  prompt_prefix = " ",
+  selection_caret = "  ",
+  entry_prefix = "  ",
+  initial_mode = "insert",
+  selection_strategy = "reset",
+  sorting_strategy = "descending",
+  layout_strategy = "horizontal",
+  layout_config = {
+    horizontal = {
+      prompt_position = "top",
+      preview_width = 0.55,
+      results_width = 0.8,
+    },
+    vertical = {
+      mirror = false,
+    },
+    width = 0.87,
+    height = 0.80,
+    preview_cutoff = 120,
+  },
+  file_sorter = require("telescope.sorters").get_fuzzy_file,
+  file_ignore_patterns = { "node_modules", ".git" },
+  generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+  path_display = { "absolute" },
+  winblend = 0,
+  border = {},
+  borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+  color_devicons = true,
+  use_less = true,
+  set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+  file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+  grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+  qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+  -- Developer configurations: Not meant for general override
+  buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+}
+
+return defaults
---
+++ lua/plugins/telescope/init.lua
@@ -0,0 +1,15 @@
+local defaults = require("plugins.telescope.defaults")
+require("telescope").setup({
+  defaults = defaults,
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+  },
+})
+
+require("telescope").load_extension("fzf")
