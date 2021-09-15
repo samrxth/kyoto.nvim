07e120334910872a3af8c250f1cbdbd6843c1b6b
samrath2007 <samrath1324@gmail.com>
2021-09-15 22:16:46 +0530

    add: tree-sitter customizations

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,1 +1,9 @@
 require("trouble").setup()
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = true,
+  },
+})
